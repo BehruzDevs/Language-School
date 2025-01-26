d583a93ef5f04e42e19519c5bbae6e34a5bad712
TestDeveloperAmazon <testdeveloperamazon@gmail.com>
2025-01-26 20:45:58 +0500

    test

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+var elMenu = document.querySelector(".menu");
+
+function handleMenuBtnClick() {
+    elMenu.classList.add("right-0");
+}
+
+function handleCloseMenu() {
+    elMenu.classList.remove("right-0");
+}
